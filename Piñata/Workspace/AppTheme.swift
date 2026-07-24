9df13bfc75208e3b44504103cf75d594e3c5dad6
Antoine Lévêque <antoine.leveque@datadoghq.com>
2026-07-24 08:26:43 +0200

    Align dark theme with Ghostty

--- Piñata/Workspace/AppTheme.swift
+++ Piñata/Workspace/AppTheme.swift
@@ -8,11 +8,11 @@ enum AppTheme {
     static let leftPanelRange: ClosedRange<CGFloat> = 200...440
     static let rightPanelRange: ClosedRange<CGFloat> = 260...520
 
-    static let background = NSColor(srgbRed: 0x1A / 255, green: 0x1C / 255, blue: 0x1E / 255, alpha: 1)
-    static let chromeBackground = NSColor(srgbRed: 0x26 / 255, green: 0x2A / 255, blue: 0x2C / 255, alpha: 1)
+    static let background = NSColor(srgbRed: 0x28 / 255, green: 0x2C / 255, blue: 0x34 / 255, alpha: 1)
+    static let chromeBackground = NSColor(srgbRed: 0x21 / 255, green: 0x25 / 255, blue: 0x2B / 255, alpha: 1)
     static let border = NSColor(srgbRed: 0x35 / 255, green: 0x3A / 255, blue: 0x3C / 255, alpha: 1)
     static let subtleBorder = NSColor(srgbRed: 0x2A / 255, green: 0x2F / 255, blue: 0x31 / 255, alpha: 1)
-    static let primaryText = NSColor(srgbRed: 0xDF / 255, green: 0xE3 / 255, blue: 0xE5 / 255, alpha: 1)
+    static let primaryText = NSColor.white
     static let secondaryText = NSColor(srgbRed: 0xB6 / 255, green: 0xBD / 255, blue: 0xC0 / 255, alpha: 1)
     static let tertiaryText = NSColor(srgbRed: 0xA6 / 255, green: 0xAE / 255, blue: 0xB2 / 255, alpha: 1)
     static let accent = NSColor(srgbRed: 0xFF / 255, green: 0x74 / 255, blue: 0x6B / 255, alpha: 1)
